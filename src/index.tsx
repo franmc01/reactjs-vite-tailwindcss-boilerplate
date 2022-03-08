48d12564de7f9a566d59aec22f762e3395d5fae1
fmarint1 <fmarin@todo1.com>
2022-03-08 08:41:15 -0500

    Fix a problem with Eslint and Prettier, add Modyo CLI

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { render } from 'react-dom'
-import './assets/styles/index.css'
-import App from './pages/App'
+import { StrictMode } from 'react';
+import { render } from 'react-dom';
+import './assets/styles/index.css';
+import App from './pages/App';
 
 render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById('root')
-)
+	<StrictMode>
+		<App />
+	</StrictMode>,
+	document.getElementById('root')
+);
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,13 @@
 const App = () => {
-  return (
-    <div className="py-16 px-4 mx-auto max-w-screen-xl sm:py-24 sm:px-6 lg:px-8">
-      <div className="text-center">
-        <h1 className="text-4xl font-semibold tracking-wide text-blue-900 uppercase">
-          Hello World!
-        </h1>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className='py-16 px-4 mx-auto max-w-screen-xl sm:py-24 sm:px-6 lg:px-8'>
+			<div className='text-center'>
+				<h1 className='text-4xl font-semibold tracking-wide text-blue-900 uppercase'>
+					Hello World!
+				</h1>
+			</div>
+		</div>
+	);
+};
 
-export default App
+export default App;
