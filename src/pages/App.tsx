31dc5ff405ec06310f8fda63bf359a9404fe23a7
Jipson Medardo Saad Intriago <jsaad@Todo1of.com.ec>
2022-03-21 14:23:20 -0500

    Remove momentarily liquid reference

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,10 @@
 import { ChangeEvent, useState } from 'react';
-import { getTypeFromLiquid } from '../utils/getTypeFromLiquid';
+// import { getTypeFromLiquid } from '../utils/getTypeFromLiquid';
 import { ObserverComponent } from '../components/ObserverComponent';
 import { PublisherComponent } from '../components/PublisherComponent';
 
 const App = () => {
-	const image = getTypeFromLiquid('{{assets.image.value}}');
+	// const image = getTypeFromLiquid('{{assets.image.value}}');
 	const [ninja, setNinja] = useState('');
 	const onChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
 		setNinja(event.target.value);
@@ -12,15 +12,11 @@ const App = () => {
 	return (
 		<div className='py-16 px-4 mx-auto max-w-screen-xl sm:py-24 sm:px-6 lg:px-8'>
 			<div className='text-center'>
-				{image != null ? (
-					image
-				) : (
-					<img
-						src='https://cloud.modyocdn.com/uploads/a7a4b683-f68a-4385-8d1c-4f3374dd2213/original/logo-tailwind_e8c14e87.png'
-						alt='logo-tailwind'
-						className='object-contain mx-auto w-96 h-48'
-					/>
-				)}
+				<img
+					src='https://cloud.modyocdn.com/uploads/a7a4b683-f68a-4385-8d1c-4f3374dd2213/original/logo-tailwind_e8c14e87.png'
+					alt='logo-tailwind'
+					className='object-contain mx-auto w-96 h-48'
+				/>
 
 				<h1 className='text-4xl font-semibold tracking-wide text-blue-900 uppercase'>
 					Hello World!
