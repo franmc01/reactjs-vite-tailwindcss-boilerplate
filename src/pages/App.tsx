f9ccef230ddaf463dbb48b20e1bfaa8ab9161822
Jipson Medardo Saad Intriago <jsaad@Todo1of.com.ec>
2022-03-22 10:34:43 -0500

    Add reactive communication between components using windowed-observable library

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,12 @@
-import { ChangeEvent, useState } from 'react';
 // import { getTypeFromLiquid } from '../utils/getTypeFromLiquid';
+import { ChangeEvent, useState } from 'react';
 import { ObserverComponent } from '../components/ObserverComponent';
 import { PublisherComponent } from '../components/PublisherComponent';
-
 const App = () => {
 	// const image = getTypeFromLiquid('{{assets.image.value}}');
 	const [ninja, setNinja] = useState('');
-	const onChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
-		setNinja(event.target.value);
+	const handleOnChange = (ev: ChangeEvent<HTMLInputElement>) => {
+		setNinja(ev.target.value);
 	};
 	return (
 		<div className='py-16 px-4 mx-auto max-w-screen-xl sm:py-24 sm:px-6 lg:px-8'>
@@ -21,17 +20,11 @@ const App = () => {
 				<h1 className='text-4xl font-semibold tracking-wide text-blue-900 uppercase'>
 					Hello World!
 				</h1>
-
-				<form
-					className='my-5'
-					onSubmit={(ev) => {
-						ev.preventDefault();
-					}}
-				>
+				<form className='my-5'>
 					<input
-						type='text'
 						className='py-2 px-3 mb-3 w-full leading-tight text-gray-700 rounded border focus:outline-none shadow appearance-none'
-						onChange={onChangeInput}
+						type='text'
+						onChange={handleOnChange}
 						placeholder='Enter the name of a famous ninja you know'
 					/>
 					<PublisherComponent ninja={ninja} />
