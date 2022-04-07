da80435a7d3be3893f672904a8333d22268be22c
Jipson Medardo Saad Intriago <jsaad@Todo1of.com.ec>
2022-04-07 09:29:44 -0500

    Refactor nav items

--- src/config/NavItems.ts
+++ src/config/NavItems.ts
@@ -1,48 +1,44 @@
 export interface NavItem {
 	label: string
-	subLabel?: string
-	children?: Array<NavItem>
 	href?: string
 }
 
+const nav: Array<NavItem> = [
+	{
+		label: 'Empezando',
+		href: '#',
+	},
+	{
+		label: 'APIs',
+		href: '#',
+	},
+	{
+		label: 'SDKs',
+		href: '#',
+	},
+	{
+		label: 'Webhooks',
+		href: '#',
+	},
+	{
+		label: 'FAQ',
+		href: '#',
+	},
+	{
+		label: 'Soporte',
+		href: '#',
+	},
+	{
+		label: 'TODO1',
+		href: '#',
+	},
+	{
+		label: 'Salida',
+		href: '#',
+	},
+]
+
 export const NAV_ITEMS = {
-	nav: [
-		{
-			label: 'Empezando',
-			href: '#',
-		},
-		{
-			label: 'APIs',
-			href: '#',
-		},
-		{
-			label: 'SDKs',
-			href: '#',
-		},
-		{
-			label: 'Webhooks',
-			href: '#',
-		},
-		{
-			label: 'FAQ',
-			href: '#',
-		},
-		{
-			label: 'Soporte',
-			href: '#',
-		},
-		{
-			label: 'TODO1',
-			href: '#',
-		},
-		{
-			label: 'Salida',
-			href: '#',
-		},
-	],
-	side: [
-		{
-			label: '',
-		},
-	],
+	nav,
+	side: {},
 }
